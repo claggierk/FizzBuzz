06b9753d475c63913b725d84187f7aebf4d839a9
Clark Phillips <clark.r.phillips@gmail.com>
2016-02-03 17:15:19 -0600

    created a class, added docstrings, separated out using modulo function vs no modulo available, you can now supply the fizz and buzz numbers (they default to 3 and 5), you can now provide the upper bound

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,52 +1,75 @@
 import unittest
 
-def FizzBuzz(use_mod):
-    increment_by_3 = 3
-    increment_by_5 = 5
+class FizzBuzz:
+    def __init__(self, upper_bound, use_mod, fizz=3, buzz=5):
+        self.fizz = fizz
+        self.buzz = buzz
+        self.upper_bound = upper_bound
+        self.use_mod = use_mod
 
-    multiple_of_3 = 3
-    multiple_of_5 = 5
+        self.fizz_buzz_output = []
+        self.fizz_multiple = self.fizz
+        self.buzz_multiple = self.buzz
 
-    output = []
+    def use_modulo(self, number):
+        """ Using modulo, determine whether a given number is:
+        divisible by the fizz argument (3 by default)
+        divisibly by the buzz argument (5 by default)
+        """
+        fizz_divisible = number % self.fizz == 0
+        buzz_divisible = number % self.buzz == 0
+        return fizz_divisible, buzz_divisible
 
-    for i in range(1, 101, 1):
-        if use_mod:
-            mod3 = i % 3 == 0
-            mod5 = i % 5 == 0
-        else:
-            # This solution does NOT use the mod operator
-            mod3 = False
-            if i == multiple_of_3:
-                mod3 = True
-                multiple_of_3 += increment_by_3
+    def no_modulo(self, number):
+        """ Without using modulo, determine whether a given number is:
+        divisible by the fizz argument (3 by default)
+        divisibly by the buzz argument (5 by default)
+        """
+        fizz_divisible = False
+        if number == self.fizz_multiple:
+            fizz_divisible = True
+            self.fizz_multiple += self.fizz
 
-            mod5 = False
+        buzz_divisible = False
+        if number == self.buzz_multiple:
+            buzz_divisible = True
+            self.buzz_multiple += self.buzz
 
-        if i == multiple_of_5:
-            mod5 = True
-            multiple_of_5 += increment_by_5
+        return fizz_divisible, buzz_divisible
 
-        result = ""
-        if mod3 and mod5:
-            result = 'FizzBuzz'
-            output.append(result)
-        elif mod3:
-            result = 'Fizz'
-            output.append(result)
-        elif mod5:
-            result = 'Buzz'
-            output.append(result)
-        else:
-            result = i
-            output.append(result)
+    def fizz_buzz(self):
+        """ for every number from 1 to upper bound (inclusive):
+        if divisible by the fizz argument (3 by default) and not the buzz argument (5 by default), print 'Fizz'
+        if divisible by the buzz argument (5 by default) and not the fizz argument (3 by default), print 'Buzz'
+        if divisible by BOTH the fizz AND buzz arguments, print 'FizzBuzz'
+        otherwise print the number
+        """
+        for number in range(1, self.upper_bound+1, 1):
+            if self.use_mod:
+                fizz_divisible, buzz_divisible = self.use_modulo(number)
+            else:
+                fizz_divisible, buzz_divisible = self.no_modulo(number)
 
-    return output
+            result = str(number) # default the result to the iteration #
+            if fizz_divisible and buzz_divisible:
+                result = 'FizzBuzz'
+            elif fizz_divisible:
+                result = 'Fizz'
+            elif buzz_divisible:
+                result = 'Buzz'
+
+            # instead of printing, adding to a list
+            self.fizz_buzz_output.append(result)
 
 class FizzBuzzTest(unittest.TestCase):
     def testModVsNoMod(self):
-        mod_output = FizzBuzz(True)
-        no_mod_output = FizzBuzz(False)
-        self.assertEqual(mod_output, no_mod_output)
+        mod = FizzBuzz(100, True)
+        mod.fizz_buzz()
+        no_mod = FizzBuzz(100,  False)
+        no_mod.fizz_buzz()
+
+        # string comparison is probably expensive...
+        self.assertEqual("\n".join(mod.fizz_buzz_output), "\n".join(no_mod.fizz_buzz_output))
 
 def main():
     unittest.main()
